f2b38371231aa7afc99b537cb263fe76b7491f79
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-01-24 20:31:31 +0100

    Added player class, no states or function yet

---
+++ PokemonSim/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonSim
+{
+    //the player character, which doubles as the game controller
+    public class Player
+    {
+        public Pokemon[] party = { null, null, null, null, null, null };
+        public string name { get; set; }
+        public int numBadges = 0;
+        public int money = 0;
+        //public List<Item> bag = new List<Item>();
+        //location
+        //state
+    }
+}
